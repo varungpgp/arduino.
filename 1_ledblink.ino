d54eb3cda844d150e9265feba7e0df9ca1cf4ed9
Delta <varunpoojary11@gmail.com>
2018-08-11 10:18:21 +0530

    small update for delay

--- 1_ledblink.ino
+++ 1_ledblink.ino
@@ -1,11 +1,12 @@
+//simple led blink
 int ledpin = 13;                                     // initialize ledpin as led @ pin 13
 void setup(){
 pinMode(ledpin,OUTPUT);                              //set ledpin as output
 }
 
-void loop(){                                         
+void loop(){
 digitalWrite(ledpin,HIGH);                           //turn on led
 delay(1000);                                         // delay of 1 sec
 digitalWrite(ledpin,LOW);                            //turn off led
-delay(1000); 
+delay(1000);
 }
